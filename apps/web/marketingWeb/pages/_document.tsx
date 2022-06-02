04d052049b93bc76dae3cebf39c8bfc2f0aadace
Darren B <68653294+Devd0@users.noreply.github.com>
2022-06-01 20:58:34 -0400

    improvs to SEO

--- apps/web/marketingWeb/pages/_document.tsx
+++ apps/web/marketingWeb/pages/_document.tsx
@@ -2,13 +2,17 @@ import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&display=swap"
           rel="stylesheet"
+        />
+        <meta
+          name="description"
+          content="The platform where developers form dream teams for and at any scale"
         />
       </Head>
       <body>

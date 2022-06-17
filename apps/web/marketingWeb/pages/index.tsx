ae23eed23f20cd38a6326c2cd6e8811825f7b2ad
Darren B <68653294+Devd0@users.noreply.github.com>
2022-06-16 21:42:59 -0400

    team section complete

--- apps/web/marketingWeb/pages/index.tsx
+++ apps/web/marketingWeb/pages/index.tsx
@@ -74,7 +74,7 @@ const Home: NextPage = () => {
             </Flex>
           </Center>
         </motion.div>
-        {/* Info #2 section */}
+        {/* Info #1 section */}
         <Box mt={{ base: 56, md: 200 }}>
           <Flex
             direction={{ base: 'column', md: 'row' }}
@@ -115,8 +115,8 @@ const Home: NextPage = () => {
             </Box>
           </Flex>
         </Box>
-        {/* Info #1 section */}
-        <Box mt={{ base: 56, md: 200 }} position="relative">
+        {/* Info #2 section */}
+        <Box mt={{ base: 24, md: 200 }} position="relative">
           <Box
             position="absolute"
             zIndex={-1}
@@ -125,160 +125,171 @@ const Home: NextPage = () => {
             left={-200}
             bgGradient="radial-gradient(closest-side, #A4BAED, transparent)"
           />
-          <Box zIndex={2} position="absolute" top={200} left={800}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.6, duration: 0.4 }}
-            >
-              <Image
-                width="262.5px"
-                height="71.4px"
-                src="/assets/profileImg5.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-          </Box>
-          <Box zIndex={2} position="absolute" top={300} left={1000}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.25, duration: 0.4 }}
-            >
-              <Image
-                width="350px"
-                height="94.5px"
-                src="/assets/profileImg11.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-           
-          </Box>
-          <Box zIndex={2} position="absolute" top={350} left={600}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 1, duration: 0.4 }}
-            >
-              <Image
-                width="268.1px"
-                height="77px"
-                src="/assets/profileImg7.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-          </Box>
-          <Box zIndex={2} position="absolute" top={250} left={200}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.8, duration: 0.4 }}
-            >
-              <Image
-                width="268.1px"
-                height="77px"
-                src="/assets/profileImg8.svg"
-                alt="user profile"
-              />
-            </motion.div> 
+          <Box display={{base: 'none', xl: 'block'}}>
+            <Box width={300} zIndex={2} position="absolute" top={200} left={800}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.6, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="300"
+                  height="100"
+                  src="/assets/profileImg5.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
+            <Box width={350} zIndex={2} position="absolute" top={300} left={1000}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.25, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="400"
+                  height="200"
+                  src="/assets/profileImg13.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            
+            </Box>
+            <Box width={300} zIndex={2} position="absolute" top={320} left={600}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 1, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="350"
+                  height="125px"
+                  src="/assets/profileImg7.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
+            <Box width={300} zIndex={2} position="absolute" top={200} left={200}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.8, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="400"
+                  height="250px"
+                  src="/assets/profileImg8.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
 
-          </Box>
-          <Box zIndex={2} position="absolute" top={580} left={1000}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.75, duration: 0.4 }}
-            >
-              <Image
-                width="268.1px"
-                height="77px"
-                src="/assets/profileImg9.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-
-          </Box>
-          <Box zIndex={2} position="absolute" top={720} left={1100}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.9, duration: 0.4 }}
-            >
-              <Image
-                width="180.6px"
-                height="53.2px"
-                src="/assets/profileImg10.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-          </Box>
-          <Box zIndex={2} position="absolute" top={850} left={500}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.25, duration: 0.4 }}
-            >
-              <Image
-                width="355.6px"
-                height="100.1px"
-                src="/assets/profileImg11.svg"
-                alt="user profile"
-              />
-            </motion.div> 
-          </Box>
-          <Box zIndex={2} position="absolute" top={730} left={250}>
-            <motion.div             
-              whileInView={{ scale: 1, opacity: 1 }}
-              initial={{ scale: 0.6, opacity: 0}}
-              transition={{ delay: 0.8, duration: 0.4 }}
-            >
-              <Image
-                width="268.1px"
-                height="77px"
-                src="/assets/profileImg12.svg"
-                alt="user profile"
-              />
-            </motion.div> 
+            </Box>
+            <Box width={250} zIndex={2} position="absolute" top={580} left={1000}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.75, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="300"
+                  height="100px"
+                  src="/assets/profileImg9.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
+            <Box width={250} zIndex={2} position="absolute" top={720} left={1100}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.9, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="250px"
+                  height="100px"
+                  src="/assets/profileImg10.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
+            <Box width={300} zIndex={2} position="absolute" top={850} left={500}>
+              <motion.div             
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.25, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="375px"
+                  height="150px"
+                  src="/assets/profileImg11.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
+            <Box width={300} zIndex={2} position="absolute" top={730} left={200}>
+              <motion.div     
+                whileInView={{ scale: 1, opacity: 1 }}
+                initial={{ scale: 0.6, opacity: 0}}
+                transition={{ delay: 0.8, duration: 0.4 }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  width="325px"
+                  height="100px"
+                  src="/assets/profileImg12.svg"
+                  alt="user profile"
+                />
+              </motion.div> 
+            </Box>
           </Box>
           <Flex align="center" justify="center" direction="column">
             <Heading
               textAlign="center"
               fontSize={{ base: '45px', sm: '50px', lg: '60px' }}
-              width="1200px"
+              maxWidth="1200px"
               size="lg"
             >
               Network, vet, and partner with developers at any scale.
             </Heading>
-            <VStack spacing={5} mt={56}>
               <Box>
-                <Image
-                  width="926.1"
-                  height="175px"
-                  src="/assets/TeamImage1.svg"
-                  alt="portt team"
-                />
+                <VStack spacing={5} mt={{base: 12, xl: 56}}>
+                  <Box width='full' maxWidth={900}>
+                    <Image
+                      width="926.1"
+                      height="175px"
+                      src="/assets/TeamImage1.svg"
+                      alt="portt team"
+                    />
+                  </Box>
+                  <Box width='80%' maxWidth={750} opacity={0.8} filter="blur(1px)">
+                    <Image
+                      width="741.3"
+                      height="140px"
+                      src="/assets/TeamImage2.svg"
+                      alt="hackathon team"
+                    />
+                  </Box>
+                  <Box width='60%' maxWidth={555} opacity={0.6} filter="blur(1px)">
+                    <Image
+                      width="555.8px"
+                      height="105px"
+                      src="/assets/TeamImage3.svg"
+                      alt="supabase team"
+                    />
+                  </Box>
+                </VStack>
               </Box>
-              <Box opacity={0.8} filter="blur(1px)">
-                <Image
-                  width="741.3"
-                  height="140px"
-                  src="/assets/TeamImage2.svg"
-                  alt="hackathon team"
-                />
-              </Box>
-              <Box opacity={0.6} filter="blur(1px)">
-                <Image
-                  width="555.8px"
-                  height="105px"
-                  src="/assets/TeamImage3.svg"
-                  alt="supabase team"
-                />
-              </Box>
-            </VStack>
           </Flex>
         </Box>
         {/* Info #3 section */}
-        <Box mt={{ base: 56, md: 200 }}>
+        <Box mt={{ base: 36, md: 200 }}>
           <Flex
             direction={{ base: 'column-reverse', md: 'row' }}
             align="center"
